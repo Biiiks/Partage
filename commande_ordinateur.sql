da5a50eed0088f5da55ef33ee244cee1df1ef78e
ESPIGUE Simon <sim.espigue@gmail.com>
2023-10-20 16:17:21 +0200

    Ordinateur

--- commande_ordinateur.sql
+++ commande_ordinateur.sql
@@ -1,22 +1,21 @@
 
-CREATE DATABASE IF NOT EXISTS commandes_ordinateur character set = "utf8" collate = "utf8_general_ci" ;
-USE commandes_20_10;
-create table clients (
+CREATE DATABASE IF NOT EXISTS commande_ordinateur character set = "utf8" collate = "utf8_general_ci" ;
+USE commande_ordinateur ;
+create table ordinateur (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
     nom VARCHAR(70) NOT NULL ,
     prenom VARCHAR(70) ,
-    email VARCHAR(100) ,
-    telephone VARCHAR(20) ,
     marque VARCHAR(20),
     modele VARCHAR(30) ,
-    prix VARCHAR(6) 
+    prix VARCHAR(6),
+    orientation TEXT 
 );
 
 DESCRIBE TABLE
 
-DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS ordinateur;
 
-DROP DATABASE IF EXISTS commandes_20_10;
+DROP DATABASE IF EXISTS commande_ordinateur ;
 
 
 
